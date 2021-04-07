1d10f566219fece5204904c35b69bcf206c1a722
Davids-dev <davisson.assuncao@gmail.com>
2021-04-07 01:23:00 -0300

    Projeto criação de tela de posts/comentários com o StringBuilders

---
+++ Travelling/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Travelling.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Travelling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Travelling.Entities;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment c1 = new Comment("Have a nice trip: "); //comentários
+            Comment c2 = new Comment("Wow that's awesome: "); //comentários
+
+            Post post = new Post
+            (
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Travelling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+            );
+
+            //para adicionar comentários no meu post
+
+            post.AddComment(c1);
+            post.AddComment(c2);
+
+
+            ///POST 2
+            Comment c3 = new Comment("Good night: "); //comentários
+            Comment c4 = new Comment("May the Force be with you: "); //comentários
+
+            Post post2 = new Post
+            (
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow!",
+                5
+            );
+
+            //para adicionar comentários no meu post
+
+            post2.AddComment(c3);
+            post2.AddComment(c4);
+
+            Console.WriteLine(post);
+            Console.WriteLine(post2);
+
+        }
+    }
+}
+ 
